eff072bddd63e74137c4f888011be4f38547a729
Morgan Miller <m.miller722@gmail.com>
2015-11-04 17:01:13 -0700

    generate post and add seed data from newage bs generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p = Post.new(title: 'New-Age Bullshit', author: 'Crystal Healer')
+p.contents = <<EOF
+The stratosphere is full of sub-atomic particles.
+
+Today, science tells us that the essence of nature is intuition. By evolving, we grow.
+
+Who are we? Where on the great story will we be aligned?
+
+The goal of bio-electricity is to plant the seeds of nature rather than bondage. You and I are dreamers of the dreamscape. Nothing is impossible.
+
+
+To wander the quest is to become one with it. Peace is the richness of stardust, and of us. We grow, we dream, we are reborn.
+
+We are at a crossroads of stardust and yearning. Reality has always been full of seekers whose hopes are nurtured by interconnectedness. Our conversations with other storytellers have led to a deepening of supra-Vedic consciousness.
+
+Consciousness consists of sub-atomic particles of quantum energy. “Quantum” means an awakening of the interstellar.
+Only a prophet of the galaxy may integrate this reintegration of being. Without chi, one cannot grow. Where there is materialism, intuition cannot thrive.
+
+Have you found your quest? Although you may not realize it, you are divine. The planet is calling to you via superpositions of possibilities. Can you hear it
+EOF
+
+p.save
+
+
+p = Post.new(title: 'More New-Age Bullshit', author: 'Subatomic Shaman')
+p.contents = <<EOF
+We exist as sub-atomic particles.
+
+Coherence is the healing of healing, and of us. This life is nothing short of a summoning unifying of life-affirming science.
+
+Yes, it is possible to sabotage the things that can eradicate us, but not without knowledge on our side.
+
+We are being called to explore the world itself as an interface between learning and aspiration. Eons from now, we dreamers will heal like never before as we are reborn by the stratosphere. Imagine a maturing of what could be.
+
+Our conversations with other entities have led to a flowering of ultra-non-dual consciousness. Humankind has nothing to lose. Throughout history, humans have been interacting with the universe via vibrations.
+
+Beauty requires exploration. The goal of electromagnetic resonance is to plant the seeds of fulfillment rather than stagnation. The infinite is overflowing with supercharged electrons.
+
+Self-actualization is the driver of hope.
+The osmosis of freedom is now happening worldwide. This mission never ends. We must learn how to lead mystical lives in the face of illusion.
+
+EOF
+
+p.save
+
+p = Post.new(title: 'Last of the New-Age Bullshit', author: 'Astro Ancestor')
+p.contents = <<EOF
+The planet is radiating meridians.
+
+Will is the growth of balance, and of us. Today, science tells us that the essence of nature is spacetime.
+
+If you have never experienced this reintegration through non-local interactions, it can be difficult to exist.
+
+We are at a crossroads of awareness and illusion. Who are we? Where on the great myth will we be recreated? We are in the midst of a life-affirming refining of joy that will tap into the quantum matrix itself.
+
+
+It can be difficult to know where to begin. Entity, look within and heal yourself. Although you may not realize it, you are consciousness-expanding.
+
+We can no longer afford to live with delusion. You may be ruled by ego without realizing it. Do not let it shatter the knowledge of your story. Without aspiration, one cannot grow.
+
+Nothing is impossible.
+Where there is selfishness, sharing cannot thrive. Only a seeker of the quantum soup may foster this current of non-locality. You must take a stand against pain.
+
+
+EOF
+
+p.save
