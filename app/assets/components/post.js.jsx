ccb42226b540b52b44b08230c243d01b492a367f
Morgan Miller <m.miller722@gmail.com>
2015-11-04 19:59:07 -0700

    add post component

---
+++ app/assets/components/post.js.jsx
@@ -0,0 +1,10 @@
+var Post = React.createClass({
+  render: function(){
+    return(
+      <div className="post">
+        <PostHeader post={this.props.post} />
+        <PostContent post={this.props.post} />
+      </div>
+    );
+  }
+});
