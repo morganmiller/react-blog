48371d9e4c4e42adf0defeca3a9a07fd42888753
Morgan Miller <m.miller722@gmail.com>
2015-11-04 20:06:42 -0700

    give the post its children

--- app/assets/components/post.js.jsx
+++ app/assets/components/post.js.jsx
@@ -1,9 +1,35 @@
+//Whole post - parent
 var Post = React.createClass({
   render: function(){
     return(
       <div className="post">
         <PostHeader post={this.props.post} />
         <PostContent post={this.props.post} />
+      </div>
+    );
+  }
+});
+
+//Header - child
+var PostHeader = React.createClass({
+  render: function() {
+    return (
+      <div className="post-header">
+        <h2>{this.props.post.title}</h2>
+        <div className="post-meta">
+          By {this.props.post.author}
+        </div>
+      </div>
+    );
+  }
+});
+
+//Content - child
+var PostContent = React.createClass({
+  render: function() {
+    return (
+      <div className="post-contents">
+        {this.props.post.contents}
       </div>
     );
   }
